48a73fb39c334330769105166f5c480f314fb46a
mikeovas <mikeovas@gmail.com>
2024-04-07 18:19:17 -0400

    job role selection
    event listener used to display input text box for other job role

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert('hi');
+const nameField = document.querySelector("[type='text']").focus();
+const otherJobRole = document.querySelector('#other-job-role');
+const jobRole = document.querySelector('#title');
+
+
+//initially hides the text box for input of other job type
+otherJobRole.style.display = "none";
+
+
+
+//event listener to  display input text box for other job type if other is slected in the dropdown menu
+jobRole.addEventListener('change', (e) => {
+    const jobChoice = e.target.value;
+      if(jobChoice === "other") {
+        otherJobRole.style.display = "block";
+        } 
+    });
+    
