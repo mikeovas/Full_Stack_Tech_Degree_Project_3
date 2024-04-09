c61ed2d527863a52da1113d868c5ea03fbd756ee
mikeovas <mikeovas@gmail.com>
2024-04-09 18:50:12 -0400

    name and email validator
    preliminary validation in place. need to proof the code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+// Declaration of variables to be used//
+
+const form = document.querySelector('form');
+
 const nameField = document.querySelector("[type='text']").focus();
 const otherJobRole = document.querySelector('#other-job-role');
 const jobRole = document.querySelector('#title');
 
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const nameHint = document.getElementById("name-hint");
+const emailHint = document.getElementById("email-hint");
 
 //initially hides the text box for input of other job type
 otherJobRole.style.display = "none";
-
-
 
 //event listener to  display input text box for other job type if other is slected in the dropdown menu
 jobRole.addEventListener('change', (e) => {
@@ -16,3 +22,30 @@ jobRole.addEventListener('change', (e) => {
         } 
     });
     
+
+
+
+// Usernames can only contain letters a-z in lowercase
+const isValidUsername = () => /^[a-z]+$/.test(nameInput.value);
+
+// Email must contain an @ symbol and a domain name
+const isValidEmail = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
+
+
+
+form.addEventListener("submit", (e) => {
+
+  const validator = (validationFunction, inputHint) => {
+    if(validationFunction()) {
+
+      inputHint.nextElementSibling.style.display = 'none';
+    } else {
+      e.preventDefault();
+      inputHint.classList.add('hint');
+      inputHint.style.display = 'block';
+    }};
+
+    validator(isValidUsername, nameHint);
+    validator(isValidEmail, emailHint);
+    
+});
