1e536c5c20fd1c3f659cfef1c2507ee1d53ce805
mikeovas <mikeovas@gmail.com>
2024-04-11 17:45:44 -0400

    formatting update

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const emailHint = document.getElementById("email-hint");
 //initially hides the text box for input of other job type
 otherJobRole.style.display = "none";
 
-//event listener to  display input text box for other job type if other is slected in the dropdown menu
+//event listener to  display input text box for other job type if other is selected as job role in the dropdown menu
 jobRole.addEventListener('change', (e) => {
     const jobChoice = e.target.value;
       if(jobChoice === "other") {
@@ -22,30 +22,24 @@ jobRole.addEventListener('change', (e) => {
         } 
     });
     
-
-
+//Text Box Input Validation Functions//
 
 // Usernames can only contain letters a-z in lowercase
 const isValidUsername = () => /^[a-z]+$/.test(nameInput.value);
 
-// Email must contain an @ symbol and a domain name
+// Valid Email must contain a name, an @ symbol and a domain name
 const isValidEmail = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
 
-
-
+//Event Listener for Name or Email Input fo Validation
 form.addEventListener("submit", (e) => {
-
   const validator = (validationFunction, inputHint) => {
     if(validationFunction()) {
-
       inputHint.style.display = 'none';
     } else {
       e.preventDefault();
       inputHint.classList.add('hint');
       inputHint.style.display = 'block';
     }};
-
     validator(isValidUsername, nameHint);
-    validator(isValidEmail, emailHint);
-    
+    validator(isValidEmail, emailHint);    
 });
