4f32e459322e6ef9779788f707fdfe0396dd5349
mikeovas <mikeovas@gmail.com>
2024-04-11 18:03:56 -0400

    reorganized

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-// Declaration of variables to be used//
 
-const form = document.querySelector('form');
 
+// Selection the name input field and giving it initial focus//
 const nameField = document.querySelector("[type='text']").focus();
+
+// Declaration of variables to be used//
+const form = document.querySelector('form');
 const otherJobRole = document.querySelector('#other-job-role');
 const jobRole = document.querySelector('#title');
-
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const nameHint = document.getElementById("name-hint");
 const emailHint = document.getElementById("email-hint");
 
-//initially hides the text box for input of other job type
+//Initially hides the text box for input of other job type
 otherJobRole.style.display = "none";
 
-//event listener to  display input text box for other job type if other is selected as job role in the dropdown menu
+//Event listener to  display input text box for other job type if other is selected as job role in the dropdown menu
 jobRole.addEventListener('change', (e) => {
     const jobChoice = e.target.value;
       if(jobChoice === "other") {
@@ -22,15 +23,15 @@ jobRole.addEventListener('change', (e) => {
         } 
     });
     
-//Text Box Input Validation Functions//
+//**Text Box Input Validation Functions**//
 
-// Usernames can only contain letters a-z in lowercase
+// Valid usernames can only contain letters a-z in lowercase
 const isValidUsername = () => /^[a-z]+$/.test(nameInput.value);
 
-// Valid Email must contain a name, an @ symbol and a domain name
+// Valid email must contain a name, an @ symbol and a domain name
 const isValidEmail = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
 
-//Event Listener for Name or Email Input fo Validation
+//Event Listener for name and email input for Validation
 form.addEventListener("submit", (e) => {
   const validator = (validationFunction, inputHint) => {
     if(validationFunction()) {
